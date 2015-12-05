0500401485be41ec75d29522bba5a69051534440
Westin <element9889@gmail.com>
2015-12-05 05:19:45 -0600

    cleanup

---
+++ zsh/history-substring.zsh
@@ -0,0 +1,9 @@
+source ~/.dotfiles/zsh/plugins/zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
+
+zmodload zsh/terminfo
+bindkey "$terminfo[kcuu1]" history-substring-search-up
+bindkey "$terminfo[kcud1]" history-substring-search-down
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+
+
